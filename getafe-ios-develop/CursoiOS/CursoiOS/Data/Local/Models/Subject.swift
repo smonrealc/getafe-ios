c9d807635a8fed0195f01b62391776a12f084c20
Sergus <s.monrealc@alumnos.urjc.es>
2019-09-24 20:48:04 +0200

    gitflow-feature-stash: viewcontroller
    Creación de pestañas

---
+++ getafe-ios-develop/CursoiOS/CursoiOS/Data/Local/Models/Subject.swift
@@ -0,0 +1,37 @@
+//
+//  Subject.swift
+//  CursoiOS
+//
+//  Created by David Jardon on 20/09/2019.
+//  Copyright © 2019 ds. All rights reserved.
+//
+
+import Foundation
+
+class Subject: CustomStringConvertible {
+    var name: String?
+    var teachers: [Teacher] = []
+    var students: [Student] = []
+    
+    var numTeachers: Int {
+        return teachers.count
+    }
+    var numStudents: Int {
+        return students.count
+    }
+    
+    // Use description var of CustomStringConvertible
+    // to print this class with custom String format
+    var description: String {
+        return "Subject data: \(String(describing: name))"
+    }
+    
+    
+    convenience init(name: String, teachers: [Teacher] = [], students: [Student] = []) {
+        self.init()
+        
+        self.name = name
+        self.teachers = teachers
+        self.students = students
+    }
+}
